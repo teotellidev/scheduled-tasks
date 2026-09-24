b37e4eba2b922f111aad93c853528cfb0379cb68
teotellidev <mat.riccitelli@gmail.com>
2026-09-24 09:00:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,81 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
+##################### Automated Birthday Wisher - Extra Hard Starting Project ######################
+#Costruire un programma Python che manda gli auguri di compleanno alle persone memorizzate all'interno del file birthday.csv
+#e dopo aver scelto un template di lettera per gli auguri, invia una mail alla persona con il messaggio di auguri
 
+#Macro Requisiti:
+# 1. Update the birthdays.csv
+# 2. Check if today matches a birthday in the birthdays.csv
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+# 4. Send the letter generated in step 3 to that person's email address.
 
-from datetime import datetime
+# ---------------------------- MODULI & IMPORTAZIONE CLASSI ------------ #
 import pandas
+import datetime as dt
+from pathlib import Path
 import random
 import smtplib
 import os
 
-# import os and use it to get the Github repository secrets
+# ---------------------------- CONSTANTI ------------------------------- #
+
 MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+PASSWORD = os.environ.get("PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+# ---------------------------- VARIABILI ------------------------------- #
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+birtday_wisher_list = []
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+
+# ---------------------------- FUNZIONI ------------------------------- #
+
+
+
+# ---------------------------- SETUP UI ------------------------------- #
+
+
+
+# ---------------------------- CORPO DEL CODICE ----------------------- #
+
+birthday_list = pandas.read_csv("birthdays.csv")
+birthday_list_dict = birthday_list.to_dict(orient="records")
+print(birthday_list_dict)
+print(type(birthday_list_dict))
+
+today = dt.datetime.now()
+
+birthday_wisher_list = [birthday_people for birthday_people in birthday_list_dict if birthday_people['day'] == today.day and birthday_people['month'] == today.month]
+#questo print sotto è solo un controllo.....può essere eliminato
+print(birthday_wisher_list)
+
+# Cartella che contiene i file
+folder = Path("letter_templates")
+
+# Ottieni tutti i file .txt della cartella
+files = list(folder.glob("*.txt"))
+
+# Seleziona un file casuale
+wisher_letter = random.choice(files)
+
+with open(wisher_letter, "r", encoding="utf-8") as f:
+    letter_content = f.read()
+
+for people in birthday_wisher_list:
+    wisher_name = people["name"]
+    custom_letter = letter_content.replace("[NAME]", wisher_name)
+    # questo print sotto è solo un controllo.....può essere eliminato
+    print(custom_letter)
+
+    with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+        #serve per mantenere la connessione criptata e sicura
         connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
+        #comando che serve per instaurare la connessione con il server
+        connection.login(user = MY_EMAIL, password = PASSWORD)
+        #comando che serve per inviare la mail, dove devo specificare la mail mittente, la mail destinataria, l'oggetto dell email ed il messaggio
+        #della mail da inviare
         connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
+            from_addr = MY_EMAIL,
+            to_addrs = people["email"],
+            msg = f"Subject: Happy Birthday\n\n{custom_letter}"
         )
+#chiudo il connettore
+connection.close()
